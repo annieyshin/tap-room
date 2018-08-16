5a42e8de4543a32625c70ae011a04be3bb2f10d9
Annie Shin and Kevin Ahn <annie@>
2018-08-16 15:39:31 -0700

    Add styling

--- src/app/models/all-kegs.ts
+++ src/app/models/all-kegs.ts
@@ -3,5 +3,5 @@ import { Keg } from './keg';
 export let kegList: Keg[] = [
   new Keg("PBR", "PBR Brand", 5, 3, 100),
   new Keg("Deschutes Beer", "Deschutes Keggery", 7, 25, 50),
-  new Keg("Tea", "The Tea Team", 16, 2, 15)
+  new Keg("Tea", "The Tea Team", 16, 7, 15)
 ];
